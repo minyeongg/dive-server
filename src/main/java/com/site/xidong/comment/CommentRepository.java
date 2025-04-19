7ab8c00da85c6438a42f2a9623792cb4caa78184
minyeongg <126530262+minyeongg@users.noreply.github.com>
2025-04-19 14:15:08 +0900

    Delete src/main/java/com/site/xidong/comment directory

--- src/main/java/com/site/xidong/comment/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.site.xidong.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface CommentRepository extends JpaRepository <Comment, Long> {
-    @Query("SELECT c FROM Comment c WHERE c.video.id = :videoId and c.id = :commentId")
-    Comment findCommentByVideoId(Long videoId, Long commentId);
-}
